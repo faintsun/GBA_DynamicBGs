6837cc788c00d0e1ad8600c25bbdb04822956cea
faintsun <sarahveith0@gmail.com>
2019-04-06 22:47:32 -0400

    moved main header stuff to main.h

--- main.c
+++ main.c
@@ -2,40 +2,16 @@
 #include <stdlib.h>
 
 // code
-#include "main.h"
 #include "Library/myLib.h"
 #include "Library/mapHandler.h"
 
-// backgrounds
-
+// maps
 #include "Maps/mapList.h"
 #include "Maps/spriteSheet.h"
 
-// variables
-unsigned int buttons;
-unsigned int oldButtons;
+// header
+#include "main.h"
 
-int hOff = 0;
-int vOff = 0;
-
-OBJ_ATTR shadowOAM[128];
-enum { ROWLOC = 0, COLLOC = 1, ROWNUMS = 2, ROWOFF = 4, COLNUMS = 6, COLOFF = 8, ROWCURS = 10, COLCURS = 12 };
-
-int dir = 0;
-int moving = 0;
-int dirTimer;
-
-AREAMAP* currMap;
-
-// prototypes
-void (*nextMove)();
-
-void init();
-void draw();
-void updateScreenLocations();
-void buttonHandler();
-void cameraHandler();
-void hideSprites();
 
 int main() {
 	init();
---
+++ main.h
@@ -0,0 +1,25 @@
+// variables
+unsigned int buttons;
+unsigned int oldButtons;
+
+int hOff = 0;
+int vOff = 0;
+
+OBJ_ATTR shadowOAM[128];
+enum { ROWLOC = 0, COLLOC = 1, ROWNUMS = 2, ROWOFF = 4, COLNUMS = 6, COLOFF = 8, ROWCURS = 10, COLCURS = 12 };
+
+int dir = 0;
+int moving = 0;
+int dirTimer;
+
+AREAMAP* currMap;
+
+// prototypes
+void (*nextMove)();
+
+void init();
+void draw();
+void updateScreenLocations();
+void buttonHandler();
+void cameraHandler();
+void hideSprites();
